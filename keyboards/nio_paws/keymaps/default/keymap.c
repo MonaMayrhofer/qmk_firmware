27584d75ad5d7be19f7be518c4accc78fa1952ce
Mona Mayrhofer <mona.mayrhofer@proton.me>
2024-07-08 15:20:42 +0200

    reorder keys

--- keyboards/nio_paws/keymaps/default/keymap.c
+++ keyboards/nio_paws/keymaps/default/keymap.c
@@ -9,18 +9,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_niopaws(
 
         //
-        KC_Q, KC_W, KC_E, KC_R, KC_T, //
-        KC_A, KC_S, KC_D, KC_F, KC_G, //
-        KC_Y, KC_X, KC_C, KC_V, KC_B, //
-        KC_SPC, KC_LCTL,              //
-        KC_LSFT,
-        KC_LALT,                            //
+        KC_Q, KC_W, KC_E, KC_R, KC_T,       //
+        KC_A, KC_S, KC_D, KC_F, KC_G,       //
+        KC_Y, KC_X, KC_C, KC_V, KC_B,       //
+        KC_1, KC_2,                         //
+        KC_3, KC_4,                         //
                                             //
-        KC_P, KC_O, KC_I, KC_U, KC_Z,       //
-        KC_L, KC_L, KC_K, KC_J, KC_H,       //
-        KC_SPC, KC_SPC, KC_SPC, KC_M, KC_N, //
-        KC_SPC, KC_SPC, KC_SPC,
-        KC_SPC, //
-        KC_SPC,
-        KC_SPC //
+        KC_Z, KC_U, KC_I, KC_O, KC_P,       //
+        KC_H, KC_J, KC_K, KC_L, KC_L,       //
+        KC_N, KC_M, KC_SPC, KC_SPC, KC_SPC, //
+        KC_1, KC_2, KC_3, KC_4,             //
+        KC_5, KC_6                          //
         )};
