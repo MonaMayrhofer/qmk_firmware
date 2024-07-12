d2584aa0933b07f27dcf2bc5c5c5ab64de594836
Mona Mayrhofer <mona.mayrhofer@proton.me>
2024-07-12 21:24:46 +0200

    further keyboard improvements

--- keyboards/nio_paws/keymaps/default/keymap.c
+++ keyboards/nio_paws/keymaps/default/keymap.c
@@ -8,6 +8,7 @@
 enum layer_names {
     NL_BASE_QWERTZ,
     NL_SPECIALS,
+    NL_PROGRAMMER,
     NL_CONTROL,
     NL_ADVANCED
 };
@@ -20,18 +21,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NL_BASE_QWERTZ] = LAYOUT_niopaws(
 
         //Left
-    DE_1,     DE_Q       ,DE_W       ,DE_E       ,DE_R,       DE_T,
-     DE_LABK,     DE_A       ,DE_S       ,DE_D       ,DE_F,       DE_G,
+    KC_ESC,     DE_Q       ,DE_W       ,DE_E       ,DE_R,       DE_T,
+     KC_LSFT,     DE_A       ,DE_S       ,DE_D       ,DE_F,       DE_G,
                DE_Y       ,DE_X       ,DE_C       ,DE_V,       DE_B,
-                                                   KC_LEFT_GUI,KC_LSFT,    KC_SPC,        _______,
+                                                   KC_LEFT_GUI, MO(NL_PROGRAMMER),    KC_SPC,        _______,
                                                                            KC_LCTL,        KC_RALT,
 
         //Right
                                 DE_Z,       DE_U,       DE_I,       DE_O,       DE_P,           DE_SS,
                                 DE_H,       DE_J,       DE_K,       DE_L,       KC_ENTER,       KC_TAB,
                                 DE_N,       DE_M,       DE_COMM,    DE_DOT,     DE_MINS,
-        KC_RCTL,       MO(NL_SPECIALS),     KC_RSFT,    _______,
-        KC_LALT,       MO(NL_CONTROL)
+        KC_RCTL,       MO(NL_CONTROL),     MO(NL_SPECIALS),    _______,
+        KC_LALT,        _______
         ),
 
 
@@ -39,36 +40,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NL_SPECIALS] = LAYOUT_niopaws(
 
         //Left
-    _______,    DE_1,       DE_2,       DE_3,       DE_4,       DE_5,
-    _______,    DE_ADIA,     _______,    _______,    KC_LEFT_BRACKET,    KC_RIGHT_BRACKET,
-               _______,    _______,    _______,    _______,    _______,
-                                                   _______,    KC_TRNS,   _______, _______,
-                                                                          _______,        _______,
+        _______    ,DE_1       ,DE_2       ,DE_3       ,DE_4       ,DE_5
+       ,_______    ,DE_ADIA    ,KC_BSPC    ,DE_UDIA    ,DE_ODIA    ,KC_DEL
+                   ,_______    ,_______    ,_______    ,_______    ,_______
+                                           ,_______    ,_______    ,_______     ,_______
+                                                                   ,_______     ,_______
 
         //Right
-                                   DE_6,       DE_7,       DE_8,       DE_9,       DE_0,     DE_ACUT,
-                                _______,    DE_UDIA,      _______,   DE_ODIA,     KC_PLUS,   DE_HASH,
-                                _______,    _______,    _______,    _______,    _______,
-        _______,    KC_TRNS,    _______,    _______,
-        _______,    _______
+                               ,DE_6       ,DE_7       ,DE_8       ,DE_9       ,DE_0        ,DE_ACUT
+                               ,KC_LEFT    ,KC_DOWN    ,KC_UP      ,KC_RIGHT   ,_______     ,_______
+                               ,_______    ,_______    ,_______    ,_______    ,_______
+       ,_______    ,KC_TRNS    ,_______    ,_______
+       ,_______    ,_______
         ),
     [NL_CONTROL] = LAYOUT_niopaws(
 
         //Left
-    _______,    DE_1,       DE_2,       DE_3,       DE_4,       DE_5,
-    _______,    KC_ESC,     KC_BSPC,    _______,    _______,    _______,
-               _______,    _______,    _______,    _______,    _______,
-                                                   _______,    KC_TRNS,   _______,  _______,
-                                                                          _______,        _______,
+        _______    ,_______    ,_______    ,_______    ,_______    ,_______
+       ,_______    ,_______    ,_______    ,_______    ,_______    ,_______
+                   ,_______    ,_______    ,_______    ,_______    ,_______
+                                           ,_______    ,_______    ,_______     ,_______
+                                                                   ,_______     ,_______
 
         //Right
-                                   DE_6,       DE_7,       DE_8,       DE_9,       DE_0,     _______,
-                                KC_LEFT,    KC_DOWN,      KC_UP,   KC_RIGHT,     _______,   _______,
-                                _______,    _______,    _______,    _______,    _______,
-        MO(NL_ADVANCED),    _______,    _______,    _______,
-        _______,    KC_TRNS
+                               ,_______    ,_______    ,_______    ,_______     ,_______    ,_______
+                               ,_______    ,_______    ,_______    ,_______     ,_______    ,_______
+                               ,_______    ,_______    ,_______    ,_______     ,_______
+       ,MO(NL_ADVANCED),_______    ,_______    ,_______
+       ,_______    ,_______
         ),
 
+    [NL_PROGRAMMER] = LAYOUT_niopaws(
+
+        //Left
+        DE_CIRC    ,DE_EXLM    ,DE_DQUO    ,_______    ,DE_DLR     ,_______
+       ,_______    ,_______    ,DE_LABK    ,DE_RABK    ,DE_PLUS    ,DE_HASH
+                   ,_______    ,_______    ,DE_PIPE    ,_______    ,_______
+                                           ,_______    ,_______    ,_______     ,_______
+                                                                   ,_______     ,_______
+
+        //Right
+                               ,DE_AMPR    ,DE_LCBR    ,DE_LPRN    ,DE_RPRN     ,DE_RCBR    ,_______
+                               ,DE_SLSH    ,_______    ,DE_LBRC    ,DE_RBRC     ,DE_EQL     ,_______
+                               ,_______    ,_______    ,_______    ,_______     ,_______
+       ,_______    ,_______    ,_______    ,_______
+       ,_______    ,_______
+        ),
     [NL_ADVANCED] = LAYOUT_niopaws(
 
         //Left
@@ -85,6 +102,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,    _______,    _______,    _______,
         _______,    KC_TRNS
         )
+
+
+
+/*
+    [NL_PROGRAMMER] = LAYOUT_niopaws(
+
+        //Left
+        _______    ,_______    ,_______    ,_______    ,_______    ,_______
+       ,_______    ,_______    ,_______    ,_______    ,_______    ,_______
+                   ,_______    ,_______    ,_______    ,_______    ,_______
+                                           ,_______    ,_______    ,_______     ,_______
+                                                                   ,_______     ,_______
+
+        //Right
+                               ,_______    ,_______    ,_______    ,_______     ,_______    ,_______
+                               ,_______    ,_______    ,_______    ,_______     ,_______    ,_______
+                               ,_______    ,_______    ,_______    ,_______     ,_______
+       ,_______    ,_______    ,_______    ,_______
+       ,_______    ,_______
+        )
+    */
 };
 void keyboard_post_init(void){
     debug_enable=true;
